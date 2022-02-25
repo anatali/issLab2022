bdb4291075b427dad48968fdf4dc67026f4c69bb
Antonio Natali <agoognat@gmail.com>
2022-02-25 19:00:49 +0100

    RadarSystem

---
+++ it.unibo.raspIntro2022/code/radarSystem/LedControl.py
@@ -0,0 +1,14 @@
+#File: LedControl.py
+import sys
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25,GPIO.OUT)
+
+for line in sys.stdin:
+   print(line)
+   v = float(line)
+   if v <= 10 :
+      GPIO.output(25,GPIO.HIGH)
+   else:
+      GPIO.output(25,GPIO.LOW)
