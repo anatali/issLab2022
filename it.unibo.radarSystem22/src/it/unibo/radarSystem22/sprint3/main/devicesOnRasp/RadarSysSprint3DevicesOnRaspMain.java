d9700d96c9324073576314493c805ac3b7a2ad99
Antonio Natali <antonio.natali@unibo.it>
2022-03-24 10:52:58 +0100

    lez22/03

--- it.unibo.radarSystem22/src/it/unibo/radarSystem22/sprint3/main/devicesOnRasp/RadarSysSprint3DevicesOnRaspMain.java
+++ it.unibo.radarSystem22/src/it/unibo/radarSystem22/sprint3/main/devicesOnRasp/RadarSysSprint3DevicesOnRaspMain.java
@@ -53,7 +53,7 @@ public class RadarSysSprint3DevicesOnRaspMain implements IApplication{
 	
 			RadarSystemConfig.tracing           = false;		
 			RadarSystemConfig.RadarGuiRemote    = true;		
-			RadarSystemConfig.protcolType       = ProtocolType.udp;		
+			RadarSystemConfig.protcolType       = ProtocolType.tcp;		
 		}
  
 	}
