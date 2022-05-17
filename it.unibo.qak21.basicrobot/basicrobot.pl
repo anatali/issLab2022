2d41f1c0ba7cf9f588633820f9dfd65b9ab5c3e0
Antonio Natali <antonio.natali@unibo.it>
2022-05-17 11:58:35 +0200

    lez17/05

---
+++ it.unibo.qak21.basicrobot/basicrobot.pl
@@ -0,0 +1,8 @@
+%====================================================================================
+% basicrobot description   
+%====================================================================================
+context(ctxbasicrobot, "localhost",  "TCP", "8020").
+ qactor( datacleaner, ctxbasicrobot, "rx.dataCleaner").
+  qactor( distancefilter, ctxbasicrobot, "rx.distanceFilter").
+  qactor( basicrobot, ctxbasicrobot, "it.unibo.basicrobot.Basicrobot").
+  qactor( envsonarhandler, ctxbasicrobot, "it.unibo.envsonarhandler.Envsonarhandler").
