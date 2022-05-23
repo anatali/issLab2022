66504736f623b08f18c129f9ce6b5999c548eb3a
Antonio Natali <agoognat@gmail.com>
2022-05-23 19:02:42 +0200

    Applicazioni 2022

---
+++ it.unibo.resourcecore/ctxresourcecore.pl
@@ -0,0 +1,5 @@
+%====================================================================================
+% ctxresourcecore description   
+%====================================================================================
+context(ctxresourcecore, "localhost",  "TCP", "8045").
+ qactor( resourcecore, ctxresourcecore, "it.unibo.resourcecore.Resourcecore").
